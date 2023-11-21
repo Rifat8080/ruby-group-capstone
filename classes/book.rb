3793b02e07587fca19195d0499818f517b8285d9
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-21 12:36:03 +0200

    Create class Book that inherits from item and initialized all attributes as visible in the diagram

---
+++ classes/book.rb
@@ -0,0 +1,15 @@
+require_relative './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state, :publish_date, :label, :title
+
+  def initiatlize(title, publisher, cover_state: 'good', published_date: nil, archived: false)
+    super(published_date, archived: archived)
+    @id = rand(0...1000)
+    @title = title
+    @publisher = publisher
+    @cover_state = cover_state
+    @publish_date = publish_date
+    @label = nil
+  end
+end
